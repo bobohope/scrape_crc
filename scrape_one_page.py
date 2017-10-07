18fe1e3abea432243454b32b2ade0259d62ed25a
huangzhongbin <bobohope@Zhongbins-MacBook-Air.local>
2017-10-07 00:02:44 -0400

    add subject field

--- scrape_one_page.py
+++ scrape_one_page.py
@@ -36,49 +36,56 @@ def start_scraping(string):
     output[2]=tier
     print('tier: ',tier)
 
+    #get Subject
+    #<span id="ContentPlaceHolderCenter_FormView1_Subject_EnLabel">Natural Sciences and Engineering</span>
+    subject=soup.find('span',id="ContentPlaceHolderCenter_FormView1_Subject_EnLabel").text
+    output[3]=subject
+    print('subject: ',subject)
+
+
     #get University
     #<span id="ContentPlaceHolderCenter_FormView1_universityLabel">Carleton University</span>
     university=soup.find('span',id="ContentPlaceHolderCenter_FormView1_universityLabel").text
-    output[3]=university
+    output[4]=university
     print('university: ',university)
 
     #get phone number
     #<span id="ContentPlaceHolderCenter_FormView1_ContactNumberLabel">613-250-2600 ext./poste 5578</span>
     phone_num=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ContactNumberLabel").text
-    output[4]=phone_num
+    output[5]=phone_num
     print('phone: ',phone_num)
 
     #get email address
     #<a id="ContentPlaceHolderCenter_FormView1_HyperLink1" href="mailto:jalbert@doe.carleton.ca" class="ui-link">jalbert@doe.carleton.ca</a>
     email=soup.find('a',id="ContentPlaceHolderCenter_FormView1_HyperLink1").text
-    output[5]=email
+    output[6]=email
     print('email: ',email)
 
     #get website
     #<a id="ContentPlaceHolderCenter_FormView1_ChairUrlLink" title="http://www.doe.carleton.ca/~jalbert/" href="http://www.doe.carleton.ca/~jalbert/" class="ui-link">Website</a>
     website=soup.find('a',id="ContentPlaceHolderCenter_FormView1_ChairUrlLink")['href']
-    output[6]=website
+    output[7]=website
     print('website: ',website)
     #get research_involve
     #<span id="ContentPlaceHolderCenter_FormView1_ResearchInvolvesLabel">Designing, fabricating, and testing photonic components that have been fabricated by laser light processing of high-performance optical materials.</span>
     research_involve=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ResearchInvolvesLabel").text
-    output[7]=research_involve
+    output[8]=research_involve
     print('research_involve: ',research_involve)
 
     #get research_relavant
     #<span id="ContentPlaceHolderCenter_FormView1_ResearchRelevanceLabel">The research will contribute to the competitiveness of the Canadian optics industry by providing new directions for photonic device research with a focus on reliability and manufacturability.</span>
     research_relavant=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ResearchRelevanceLabel").text
-    output[8]=research_relavant
+    output[9]=research_relavant
     print('research_relavant: ',research_relavant)
 
     #get story_title
     #<span id="ContentPlaceHolderCenter_FormView1_StoryTitleLabel">Using Laser Light to Create Optical Components</span>
     story_title=soup.find('span',id="ContentPlaceHolderCenter_FormView1_StoryTitleLabel").text
-    output[9]=story_title
+    output[10]=story_title
     print('story_title: ',story_title)
     #get story_content
     story=soup.find('div',id="ContentPlaceHolderCenter_FormView1_PanelStory").text
-    output[10]=story
+    output[11]=story
     print('story: ',story)
 
     #print everything
