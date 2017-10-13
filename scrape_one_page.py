348e55aa9af58cfbd494a4e256d9fac5dd072f80
huangzhongbin <bobohope@Zhongbins-MacBook-Air.local>
2017-10-13 19:35:02 -0400

    update

--- scrape_one_page.py
+++ scrape_one_page.py
@@ -15,23 +15,33 @@ def parse_name(string):
     del name_split[-1]
     firstname=" ".join(name_split)
     return [lastname, firstname]
-def start_scraping(string):
+
+def get_html(string):
     #start a session (if you have no idea what is a session, google plz)
     s = requests.session()
     #get the page
     rq= s.get(string)
-    url = []
-    soup = ''
-    output=[None]*14
-    all_content=[]
+    s.close()
     soup = BeautifulSoup(rq.text,'html.parser')
+    return soup
+
+def checkstatus(soup):
+    if soup.find('span',id="ContentPlaceHolderCenter_FormView1_ChairTitleLabel") is None:
+        return False
+    else:
+        return True
+
+def start_scraping(soup):
+
+    output=[None]*14
+
     #print html file for debug only
     #print(soup.prettify())
     #get name
     #<h1 id="ContentPlaceHolderCenter_H1" class="border-bottom-none">Jacques Albert</h1>
     name = soup.find('h1').text
     output[0] = name
-    print('name: ',name)
+    #print('name: ',name)
     #TODO:parse the full name into lastname and first name
     name_split=parse_name(name)
     output[1]=name_split[0]
@@ -51,38 +61,38 @@ def start_scraping(string):
     #<span id="ContentPlaceHolderCenter_FormView1_ChairTitleLabel" class="bold">Canada Research Chair in Advanced Photonic Components</span>
     chair_title = soup.find('span',id="ContentPlaceHolderCenter_FormView1_ChairTitleLabel").text
     output[3]=chair_title
-    print('chair_title: ',chair_title)
+    #print('chair_title: ',chair_title)
 
     #get Tier
     #<span id="ContentPlaceHolderCenter_FormView1_TierLabel">Tier 1</span>
     tier=soup.find('span',id="ContentPlaceHolderCenter_FormView1_TierLabel").text
     output[4]=tier
-    print('tier: ',tier)
+    #print('tier: ',tier)
 
     #get Subject
     #<span id="ContentPlaceHolderCenter_FormView1_Subject_EnLabel">Natural Sciences and Engineering</span>
     subject=soup.find('span',id="ContentPlaceHolderCenter_FormView1_Subject_EnLabel").text
     output[5]=subject
-    print('subject: ',subject)
+    #print('subject: ',subject)
 
 
     #get University
     #<span id="ContentPlaceHolderCenter_FormView1_universityLabel">Carleton University</span>
     university=soup.find('span',id="ContentPlaceHolderCenter_FormView1_universityLabel").text
     output[6]=university
-    print('university: ',university)
+    #print('university: ',university)
 
     #get phone number
     #<span id="ContentPlaceHolderCenter_FormView1_ContactNumberLabel">613-250-2600 ext./poste 5578</span>
     phone_num=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ContactNumberLabel").text
     output[7]=phone_num
-    print('phone: ',phone_num)
+    #print('phone: ',phone_num)
 
     #get email address
     #<a id="ContentPlaceHolderCenter_FormView1_HyperLink1" href="mailto:jalbert@doe.carleton.ca" class="ui-link">jalbert@doe.carleton.ca</a>
     email=soup.find('a',id="ContentPlaceHolderCenter_FormView1_HyperLink1").text
     output[8]=email
-    print('email: ',email)
+    #print('email: ',email)
 
     #get website
     #<a id="ContentPlaceHolderCenter_FormView1_ChairUrlLink" title="http://www.doe.carleton.ca/~jalbert/" href="http://www.doe.carleton.ca/~jalbert/" class="ui-link">Website</a>
@@ -90,54 +100,54 @@ def start_scraping(string):
     website_archer=soup.find('a',id="ContentPlaceHolderCenter_FormView1_ChairUrlLink")
     if website_archer is not None:
         output[9]=website_archer['href']
-    print('website: ',output[9])
+    #print('website: ',output[9])
     #get research_involve
     #<span id="ContentPlaceHolderCenter_FormView1_ResearchInvolvesLabel">Designing, fabricating, and testing photonic components that have been fabricated by laser light processing of high-performance optical materials.</span>
     research_involve=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ResearchInvolvesLabel").text
     output[10]=research_involve
-    print('research_involve: ',research_involve)
+    #print('research_involve: ',research_involve)
 
     #get research_relavant
     #<span id="ContentPlaceHolderCenter_FormView1_ResearchRelevanceLabel">The research will contribute to the competitiveness of the Canadian optics industry by providing new directions for photonic device research with a focus on reliability and manufacturability.</span>
     research_relavant=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ResearchRelevanceLabel").text
     output[11]=research_relavant
-    print('research_relavant: ',research_relavant)
+    #print('research_relavant: ',research_relavant)
 
     #get story_title
     #<span id="ContentPlaceHolderCenter_FormView1_StoryTitleLabel">Using Laser Light to Create Optical Components</span>
     story_title=soup.find('span',id="ContentPlaceHolderCenter_FormView1_StoryTitleLabel").text
     output[12]=story_title
-    print('story_title: ',story_title)
+    #print('story_title: ',story_title)
     #get story_content
     story=soup.find('div',id="ContentPlaceHolderCenter_FormView1_PanelStory").text
     import re
     story = re.sub('\s+', ' ', story)
     output[13]=story
-    print('story: ',story)
+    #print('story: ',story)
 
     #print everything
     print(output)
+    return output
 
-def checkstatus(string):
-    import requests
-    s = requests.session()
-    rq= s.get(string)
-    soup = ''
-    soup = BeautifulSoup(rq.text,'html.parser')
-    chair_title = soup.find('h1').text
-    request = requests.get(string)
-    if request.status_code == 200:
-        if  len(chair_title.split()) != 1:
-            return True
-    return False
+
+
+def write_to_csv(info):
+    import csv
+    with open('output.csv', 'a') as csvfile:
+        writer = csv.writer(csvfile,dialect='excel')
+        writer.writerow(info)
 
 if __name__ == "__main__":
     url_base = 'http://www.chairs-chaires.gc.ca/chairholders-titulaires/profile-eng.aspx?profileId='
-    # print(checkstatus(url_base+str(1070)))
-    # start_scraping(url_base+str(1070))
+    #soup = get_html(url_base+str(1070))
+    #print(checkstatus(soup))
+    #outout = start_scraping(soup)
+    #write_to_csv(outout)
     for i in range(1, 1000):
-        if checkstatus(url_base+str(i)) == True:
-            print(i)
-            start_scraping(url_base+str(i))
+        soup=get_html(url_base+str(i))
+        if checkstatus(soup) == True:
+            print("start_scraping--", i)
+            info=start_scraping(soup)
+            write_to_csv(info)
             #this is VERY IMPORTANT to prevent being blocked!!!!!!!!
-            time.sleep(2)
+        time.sleep(1)
