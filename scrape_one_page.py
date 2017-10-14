29b91f0a01e873eb9c3e9a3bd81d5d24022fdca7
bobohope <zhongbin@ualberta.ca>
2017-10-13 20:24:16 -0400

    add if

--- scrape_one_page.py
+++ scrape_one_page.py
@@ -71,27 +71,31 @@ def start_scraping(soup):
 
     #get Subject
     #<span id="ContentPlaceHolderCenter_FormView1_Subject_EnLabel">Natural Sciences and Engineering</span>
-    subject=soup.find('span',id="ContentPlaceHolderCenter_FormView1_Subject_EnLabel").text
-    output[5]=subject
+    subject=soup.find('span',id="ContentPlaceHolderCenter_FormView1_Subject_EnLabel")
+    if subject is not None:
+        output[5]=subject.text
     #print('subject: ',subject)
 
 
     #get University
     #<span id="ContentPlaceHolderCenter_FormView1_universityLabel">Carleton University</span>
-    university=soup.find('span',id="ContentPlaceHolderCenter_FormView1_universityLabel").text
-    output[6]=university
+    university=soup.find('span',id="ContentPlaceHolderCenter_FormView1_universityLabel")
+    if university is not None:
+        output[6]=university.text
     #print('university: ',university)
 
     #get phone number
     #<span id="ContentPlaceHolderCenter_FormView1_ContactNumberLabel">613-250-2600 ext./poste 5578</span>
-    phone_num=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ContactNumberLabel").text
-    output[7]=phone_num
+    phone_num=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ContactNumberLabel")
+    if phone_num is  not None:
+        output[7]=phone_num.text
     #print('phone: ',phone_num)
 
     #get email address
     #<a id="ContentPlaceHolderCenter_FormView1_HyperLink1" href="mailto:jalbert@doe.carleton.ca" class="ui-link">jalbert@doe.carleton.ca</a>
-    email=soup.find('a',id="ContentPlaceHolderCenter_FormView1_HyperLink1").text
-    output[8]=email
+    email=soup.find('a',id="ContentPlaceHolderCenter_FormView1_HyperLink1")
+    if email is not None:
+        output[8]=email.text
     #print('email: ',email)
 
     #get website
@@ -103,26 +107,31 @@ def start_scraping(soup):
     #print('website: ',output[9])
     #get research_involve
     #<span id="ContentPlaceHolderCenter_FormView1_ResearchInvolvesLabel">Designing, fabricating, and testing photonic components that have been fabricated by laser light processing of high-performance optical materials.</span>
-    research_involve=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ResearchInvolvesLabel").text
-    output[10]=research_involve
+    research_involve=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ResearchInvolvesLabel")
+    if research_involve is not None:
+        output[10]=research_involve.text
     #print('research_involve: ',research_involve)
 
     #get research_relavant
     #<span id="ContentPlaceHolderCenter_FormView1_ResearchRelevanceLabel">The research will contribute to the competitiveness of the Canadian optics industry by providing new directions for photonic device research with a focus on reliability and manufacturability.</span>
-    research_relavant=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ResearchRelevanceLabel").text
-    output[11]=research_relavant
+    research_relavant=soup.find('span',id="ContentPlaceHolderCenter_FormView1_ResearchRelevanceLabel")
+    if  research_relavant is not None:
+        output[11]=research_relavant.text
     #print('research_relavant: ',research_relavant)
 
     #get story_title
     #<span id="ContentPlaceHolderCenter_FormView1_StoryTitleLabel">Using Laser Light to Create Optical Components</span>
-    story_title=soup.find('span',id="ContentPlaceHolderCenter_FormView1_StoryTitleLabel").text
-    output[12]=story_title
+    story_title=soup.find('span',id="ContentPlaceHolderCenter_FormView1_StoryTitleLabel")
+    if story_title is not None:
+        output[12]=story_title.text
     #print('story_title: ',story_title)
     #get story_content
-    story=soup.find('div',id="ContentPlaceHolderCenter_FormView1_PanelStory").text
-    import re
-    story = re.sub('\s+', ' ', story)
-    output[13]=story
+    story=soup.find('div',id="ContentPlaceHolderCenter_FormView1_PanelStory")
+    if story is not None:
+        import re
+        storya=story.text
+        storya= re.sub('\s+', ' ', storya)
+        output[13]=storya
     #print('story: ',story)
 
     #print everything
@@ -143,11 +152,11 @@ if __name__ == "__main__":
     #print(checkstatus(soup))
     #outout = start_scraping(soup)
     #write_to_csv(outout)
-    for i in range(1, 1000):
+    for i in range(3504, 5000):
         soup=get_html(url_base+str(i))
         if checkstatus(soup) == True:
             print("start_scraping--", i)
             info=start_scraping(soup)
             write_to_csv(info)
             #this is VERY IMPORTANT to prevent being blocked!!!!!!!!
-        time.sleep(1)
+        time.sleep(0.5)
